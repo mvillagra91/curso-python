772dee073a7e2d59f85e466bb2684ed9aa2190c5
Mario Villagra <mvillagra.lorca@gmail.com>
2024-11-19 13:02:24 -0300

    added variables python

---
+++ 01_variables.py
@@ -0,0 +1,21 @@
+#Variables
+#Buenas practicas -> Espacios con guion bajo para declarar variables
+
+#String
+my_string_variable = "Mi Variable"
+print(my_string_variable)
+
+#Int
+my_int_variable = 1
+print(my_int_variable)
+
+#boolean
+my_boolean_variable = True
+print(my_boolean_variable)
+
+#imprimir muchos datos con un print (se separa con ,)
+print(my_boolean_variable, my_int_variable, my_string_variable)
+
+#declarar variables en una sola linea (Tener precaución no recomendado en cantidades grandes)
+nombre, apellido, edad, estatura = "nombre", "villagra", 33, 1.66
+print(nombre, apellido, edad, estatura)
